28a6a6d4d8cd6ed64d8332e5fd6a81d1ebff9d84
Zach Huxford <zachbwh@gmail.com>
2022-07-10 12:25:49 +1200

    add stateless widgets from tutorial

--- daily_zach/lib/main.dart
+++ daily_zach/lib/main.dart
@@ -12,9 +12,70 @@ import 'package:flutter/material.dart';
 ///           Profile Widget
 /// -----------------------------------
 
+class Profile extends StatelessWidget {
+  final logoutAction;
+  final String name;
+  final String picture;
+
+  const Profile(this.logoutAction, this.name, this.picture);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Container(
+          width: 150,
+          height: 150,
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.blue, width: 4.0),
+            shape: BoxShape.circle,
+            image: DecorationImage(
+              fit: BoxFit.fill,
+              image: NetworkImage(picture ?? ''),
+            ),
+          ),
+        ),
+        const SizedBox(height: 24.0),
+        Text('Name: $name'),
+        const SizedBox(height: 48.0),
+        ElevatedButton(
+          onPressed: () {
+            logoutAction();
+          },
+          child: const Text('Logout'),
+        ),
+      ],
+    );
+  }
+}
+
 /// -----------------------------------
 ///            Login Widget
 /// -----------------------------------
+
+class Login extends StatelessWidget {
+  final loginAction;
+  final String loginError;
+
+  const Login(this.loginAction, this.loginError);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        ElevatedButton(
+          onPressed: () {
+            loginAction();
+          },
+          child: const Text('Login'),
+        ),
+        Text(loginError ?? ''),
+      ],
+    );
+  }
+}
 
 /// -----------------------------------
 ///                 App
